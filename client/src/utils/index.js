6c5b5b8e054bed010f861d0d3a402aaff9fc8bdd
mystificator <vipultandon.business@gmail.com>
2024-12-13 21:47:18 +0530

    Added card display to homepage

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,11 +1,15 @@
-import { surpriseMePrompts } from "../constants";
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constants';
 
-export function getRandomPrompts (prompt) {
+export function getRandomPrompts(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
 
-  if (randomPrompt === prompt) return getRandomPrompts(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
-};
+}
 
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
