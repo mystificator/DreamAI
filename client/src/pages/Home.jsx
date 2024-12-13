6848496475631feac27648f14199dc48cdf582cb
mystificator <vipultandon.business@gmail.com>
2024-12-13 21:47:52 +0530

    Added search functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,34 +17,34 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState("");
-  // const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
-  // const fetchPosts = async () => {
-  //   setLoading(true);
+  const fetchPosts = async () => {
+    setLoading(true);
 
-  //   try {
-  //     const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-  //     if (response.ok) {
-  //       const result = await response.json();
-  //       setAllPosts(result.data.reverse());
-  //     }
-  //   } catch (err) {
-  //     alert(err);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   fetchPosts();
-  // }, []);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
@@ -76,12 +76,12 @@ const Home = () => {
 
       <div className="mt-16">
         <FormField
-        // labelName="Search posts"
-        // type="text"
-        // name="text"
-        // placeholder="Search something..."
-        // value={searchText}
-        // handleChange={handleSearchChange}
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search something..."
+          value={searchText}
+          handleChange={handleSearchChange}
         />
       </div>
 
