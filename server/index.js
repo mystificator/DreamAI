a5e912da0312c2420ca5203b02bb1219bc2f0c6d
mystificator <vipultandon.business@gmail.com>
2024-12-14 20:08:41 +0530

    updated cors origin url

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://dream-ai-nine.vercel.app"],
+  origin: ["http://localhost:5173", "https://dream-ai-umber.vercel.app"],
   methods: ["GET", "POST"]
 }
 
